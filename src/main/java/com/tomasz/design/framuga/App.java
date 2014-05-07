49010f516d166f1522f5318f8b830d9442d76798
Tomasz Kuśmierczyk <tomaszkusmierczyk@letsweb.biz>
2014-05-07 12:01:38 +0200

    file copying

--- src/main/java/com/tomasz/design/framuga/App.java
+++ src/main/java/com/tomasz/design/framuga/App.java
@@ -1,6 +1,8 @@
 package com.tomasz.design.framuga;
 
-import org.apache.commons.configuration.ConfigurationException;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,8 +14,18 @@ public class App {
     
     public static final Logger LOGGER = LoggerFactory.getLogger(App.class);
     
-    public static void main(String[] args) throws ConfigurationException, InterruptedException {
-        LOGGER.info("Logging hello world.");
-        LOGGER.debug("Logging hello world.");
+    public static void main(String[] args) throws Exception {
+        LOGGER.info("Starting application.");
+        CamelContext camelContext = new DefaultCamelContext();
+        camelContext.addRoutes(new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+                from("file:data/inbox?noop=true").to("file:data/outbox");
+            }
+        });
+        camelContext.start();
+        Thread.sleep(10000);
+        camelContext.stop();
     }
 }
