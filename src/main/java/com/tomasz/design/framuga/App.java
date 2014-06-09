c4614b7a105e109f088e77c40fe30cfd846f232e
Tomasz Kuśmierczyk <tomaszkusmierczyk@letsweb.biz>
2014-06-09 17:19:21 +0200

    camel stream exercise

--- src/main/java/com/tomasz/design/framuga/App.java
+++ src/main/java/com/tomasz/design/framuga/App.java
@@ -1,7 +1,27 @@
 package com.tomasz.design.framuga;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Map;
 import javax.jms.ConnectionFactory;
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
@@ -10,6 +30,8 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.model.RouteDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -28,12 +50,12 @@ public class App {
         ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
         Operatorable operatorable = ac.getBean("myOperator", DivisionOperator.class);
         System.out.println(operatorable.devide(12, 6));
-        CamelContext camelContext = new DefaultCamelContext();
+        ModelCamelContext modelCamelContext = new DefaultCamelContext();
 
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
-        camelContext.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        modelCamelContext.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 
-        camelContext.addRoutes(new RouteBuilder() {
+        modelCamelContext.addRoutes(new RouteBuilder() {
 
             @Override
             public void configure() throws Exception {
@@ -68,19 +90,35 @@ public class App {
                         System.out.println(request.getLocalAddr());
                     }
                 }).to("http://localhost:8084/lukasfloorspring/?bridgeEndpoint=true");
-                from("jetty:http://localhost:9999/lukasfloorspring/resources/styles/standard.css").process(new Processor() {
+//                from("jetty:http://localhost:9999/lukasfloorspring/resources/styles/standard.css").process(new Processor() {
+//
+//                    @Override
+//                    public void process(Exchange exchange) throws Exception {
+//                        System.out.println(exchange.getOut().getHeaders());
+//                    }
+//                }).to("http://localhost:8084/lukasfloorspring/resources/styles/standard.css?bridgeEndpoint=true");
 
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        System.out.println(exchange.getOut().getHeaders());
-                    }
-                }).to("http://localhost:8084/lukasfloorspring/resources/styles/standard.css?bridgeEndpoint=true");
+//                from("stream:url?url=http://localhost:8084/lukasfloorspring&scanStream=true&encoding=UTF-8").process(new Processor() {
+//
+//                    @Override
+//                    public void process(Exchange exchange) throws Exception {
+//                    }
+//                }).to("stream:out");
+                
+//                from("servlet:///localhost:8084/lukasfloorspring/").process(new Processor() {
+//
+//                    @Override
+//                    public void process(Exchange exchange) throws Exception {
+//                        System.out.println(exchange.getOut().getHeaders());
+//                    }
+//                }).to("stream:out");
             }
-        });
-        camelContext.start();
-//        Thread.sleep(50000);
-//        camelContext.stop();
+        }
+        );
+
+//        modelCamelContext.addRouteDefinition(rd);
+        modelCamelContext.start();
+        Thread.sleep(50000);
+        modelCamelContext.stop();
     }
-
-
 }
